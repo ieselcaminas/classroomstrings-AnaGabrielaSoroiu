c29248fc0e104d1b0ddec2b2b658ad88d8663a5c
anasor2 <anasor2@alu.edu.gva.es>
2024-11-15 10:33:05 +0100

    Palindromo

--- src/main/Palindromo.java
+++ src/main/Palindromo.java
@@ -13,7 +13,7 @@ public class Palindromo {
     }
 
     public static void main(String[] args) {
-        String cadena = "abba";
+        String cadena = "abca";
 
         System.out.println(esPalindromo(cadena));
     }

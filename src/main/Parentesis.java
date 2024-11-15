991ac08e04d4585658feb751c08b589cb1b91d17
anasor2 <anasor2@alu.edu.gva.es>
2024-11-15 10:30:38 +0100

    Parentesis

--- src/main/Parentesis.java
+++ src/main/Parentesis.java
@@ -1,26 +1,26 @@
 public class Parentesis {
     public static boolean esParentizada (String cadena) {
         boolean esParentizada = false;
+        int contador = 0;
 
         for (int i = 0; i < cadena.length(); i++) {
             if (cadena.charAt(i) == '(') {
-                for (int j = i + 1; j < cadena.length(); j++) {
-                    if (cadena.charAt(j) == ')') {
-                        esParentizada = true;
-                    } else {
-                        esParentizada = false;
-                    }
-                }
-            } else {
-                esParentizada = false;
-                break;
+                contador++;
+            } else if (cadena.charAt(i) == ')') {
+                contador--;
             }
+        }
+
+        if (contador == 0) {
+            esParentizada = true;
+        } else {
+            esParentizada = false;
         }
         return esParentizada;
     }
 
     public static void main(String[] args) {
-        String cadena = "una )cadena (mal (parentizada)";
+        String cadena = "Esto (es (un ejemplo) (de) una (cadena bien) parentizada)";
 
         System.out.println(esParentizada(cadena));
     }
